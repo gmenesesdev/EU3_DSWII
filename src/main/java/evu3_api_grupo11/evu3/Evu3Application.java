e5272f6c7e903df8a05e1aa498bb3d882713d1b2
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-12-18 01:48:52 -0300

    First commit

---
+++ src/main/java/evu3_api_grupo11/evu3/Evu3Application.java
@@ -0,0 +1,13 @@
+package evu3_api_grupo11.evu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evu3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evu3Application.class, args);
+	}
+
+}
---
+++ src/test/java/evu3_api_grupo11/evu3/Evu3ApplicationTests.java
@@ -0,0 +1,13 @@
+package evu3_api_grupo11.evu3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evu3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
